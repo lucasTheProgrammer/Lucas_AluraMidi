96626d11f0660ab13edcf0ecfdde047a77f9df13
lucasTheProgrammer <134975545+lucasTheProgrammer@users.noreply.github.com>
2023-09-18 19:06:25 -0300

    eyjyj

--- main.js
+++ main.js
@@ -17,3 +17,33 @@ function tocaSomTim () {
    
 }
 document.querySelector('.tecla_tim').onclick = tocaSomTim
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+   
+}
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+   
+}
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+   
+}
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play();
+   
+}
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+   
+}
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+   
+}
