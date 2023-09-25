4eed2934ea65d954974249b1a712b43dce9f467e
lucasTheProgrammer <134975545+lucasTheProgrammer@users.noreply.github.com>
2023-09-25 19:48:53 -0300

    9ý9dfeaf

--- main.js
+++ main.js
@@ -7,19 +7,9 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
 
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[0].onclick = tocaSomPom;
 
-listaDeTeclas[1].onclick = tocaSomClap;
-
-listaDeTeclas[2].onclick = tocaSomTim;
-
-listaDeTeclas[3].onclick = tocaSomPuff;
-
-listaDeTeclas[4].onclick = tocaSomSplash;
-
-listaDeTeclas[5].onclick = tocaSomToim;
-
-listaDeTeclas[6].onclick = tocaSomPsh;
-
-listaDeTeclas[7].onclick = tocaSomTic;
-
-listaDeTeclas[8].onclick = tocaSomTom;
+    contador = contador + 1
+    console.log(contador);
+}
