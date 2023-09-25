2c7b17812817e284a8e8cb9293d69bb8e83535ec
lucasTheProgrammer <134975545+lucasTheProgrammer@users.noreply.github.com>
2023-09-25 19:36:52 -0300

    dhdh

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert ('terminei o 3 oque faço agora?')
+
 function tocaSomPom () {
     document.querySelector ('#som_tecla_pom').play();
 }
@@ -7,11 +7,19 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
 
+
 listaDeTeclas[0].onclick = tocaSomClap;
+
 listaDeTeclas[0].onclick = tocaSomTim;
+
 listaDeTeclas[0].onclick = tocaSomPuff;
+
 listaDeTeclas[0].onclick = tocaSomSplash;
+
 listaDeTeclas[0].onclick = tocaSomToim;
+
 listaDeTeclas[0].onclick = tocaSomPsh;
+
 listaDeTeclas[0].onclick = tocaSomTic;
+
 listaDeTeclas[0].onclick = tocaSomTom;
