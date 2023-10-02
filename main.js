a0aad4cf22c6ce02a60e3352c1d1628bdf0ca61b
lucasTheProgrammer <134975545+lucasTheProgrammer@users.noreply.github.com>
2023-10-02 19:08:06 -0300

    up

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[0].onclick = tocaSomPom;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSom;
+    listaDeTeclas[contador].onclick = function () 
+
+    tocaSom('#som_tecla_pom');
 
     contador = contador + 1
 
