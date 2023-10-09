8c8d83fd7e279b8cc6e2016b11e79a7e71d6647b
lucasTheProgrammer <134975545+lucasTheProgrammer@users.noreply.github.com>
2023-10-09 19:18:27 -0300

    qewf

--- main.js
+++ main.js
@@ -1,51 +1,39 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-const listaDeTeclas = document.querySelectorAll('input[type=button]');
-const inputTel = document.querySelector('input[type=tel]');
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-for (let indice = 0; indice < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-
-
-
-    const tecla = listaDeTeclas[indice];
-    const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrumento}`;
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-          
-         
-      
-      
+
     tecla.onkeydown = function (evento) {
-          console.log(evento.code)
-        if (evento.code = 'Space' )  {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
-
         }
-        
-     
+
     }
-       
-       
+
     tecla.onkeyup = function () {
-          
         tecla.classList.remove('ativa');
-        
-       
     }
-
-
 
 }
-
-
- 
- 
-
